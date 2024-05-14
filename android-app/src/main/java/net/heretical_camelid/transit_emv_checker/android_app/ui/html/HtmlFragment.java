78c224f46ee36e6d4ab2f65704a0b65a43cd36c0
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-05-14 17:48:24 +0800

    It is now possible to identify which HTML fragment is visible

--- android-app/src/main/java/net/heretical_camelid/transit_emv_checker/android_app/ui/html/HtmlFragment.java
+++ android-app/src/main/java/net/heretical_camelid/transit_emv_checker/android_app/ui/html/HtmlFragment.java
@@ -1,24 +1,24 @@
 package net.heretical_camelid.transit_emv_checker.android_app.ui.html;
 
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import net.heretical_camelid.transit_emv_checker.android_app.MainActivity;
 import net.heretical_camelid.transit_emv_checker.android_app.R;
-import net.heretical_camelid.transit_emv_checker.android_app.databinding.FragmentHomeBinding;
 import net.heretical_camelid.transit_emv_checker.android_app.databinding.FragmentHtmlBinding;
 
 public class HtmlFragment extends Fragment {
 
     private FragmentHtmlBinding binding;
+    private HtmlViewModel m_htmlViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,14 +36,10 @@ public class HtmlFragment extends Fragment {
         sb.append("<p>root.id=" + root.getId() + " " + root.getTag() + "</p>");
         sb.append("<p>parent.id=" + parent.getId() + " " + parent.getTag() + "</p>");
         sb.append("</body></html>");
-        HtmlViewModel htmlViewModel =
-                new ViewModelProvider(this).get(HtmlViewModel.class);
-        htmlViewModel.setText(sb.toString());
+        m_htmlViewModel = new ViewModelProvider(this).get(HtmlViewModel.class);
+        m_htmlViewModel.setText(sb.toString());
 
-
-
-
-        htmlViewModel.getText().observe(
+        m_htmlViewModel.getText().observe(
                 getViewLifecycleOwner(),
                 observableWebViewWrapper::loadHtmlText
         );
@@ -55,5 +51,22 @@ public class HtmlFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        NavController navController = Navigation.findNavController(
+                (MainActivity) getActivity(),
+                R.id.nav_host_fragment_activity_main
+        );
+        NavDestination currentDestination = navController.getCurrentDestination();
+        assert currentDestination != null;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><body>");
+        sb.append("<p>destination.id: " + currentDestination.getId() + "</p>");
+        sb.append("<p>destination.label: " + currentDestination.getLabel() + "</p>");
+        sb.append("</body></html>");
+        m_htmlViewModel.setText(sb.toString());
     }
 }

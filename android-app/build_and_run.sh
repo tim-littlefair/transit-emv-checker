0a377bb2c28b7f9b67568afda9f1041a2c7b577c
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-05-23 14:17:23 +0800

    GH #13: Integrated com.jaredsburrows:gradle-license-plugin
    This allows Gradle to collect license files relating to dependencies and generate a report to display them

--- android-app/build_and_run.sh
+++ android-app/build_and_run.sh
@@ -6,7 +6,7 @@ version=0.1.0
 # Using the gradlew file generated as part of the IntelliJ IDEA project
 gradle=./gradlew
 
-$gradle --warning-mode all :android-app:clean :android-app:collectLicenseFile :android-app:build
+$gradle --warning-mode all :android-app:clean :android-app:build
 gradle_status=$?
 
 # TODO: Install on an android device and run

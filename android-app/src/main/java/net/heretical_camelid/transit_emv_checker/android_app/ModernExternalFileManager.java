c3feb104cdb27a45d38767191ebb48f7ca24d6b5
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-05-31 13:23:24 +0800

    GH #15: MEFM now uses registerForActivityResult
    The GH ticket will be closed when this branch is merged, but a new separate ticket will be raised to cover improving the user experience

--- android-app/src/main/java/net/heretical_camelid/transit_emv_checker/android_app/ModernExternalFileManager.java
+++ android-app/src/main/java/net/heretical_camelid/transit_emv_checker/android_app/ModernExternalFileManager.java
@@ -2,6 +2,7 @@ package net.heretical_camelid.transit_emv_checker.android_app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.UriPermission;
 import android.net.Uri;
 import android.provider.DocumentsContract;
 import androidx.activity.result.ActivityResult;
@@ -13,13 +14,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.List;
 import java.util.TreeMap;
 
 public class ModernExternalFileManager extends ExternalFileManagerBase {
     static final Logger LOGGER = LoggerFactory.getLogger(ModernExternalFileManager.class);
 
+    static final int SAVEDIR_FLAGS_READ_WRITE = (
+        Intent.FLAG_GRANT_WRITE_URI_PERMISSION |
+        Intent.FLAG_GRANT_READ_URI_PERMISSION
+    );
+
     final private MainActivity m_mainActivity;
-    Uri m_xmlSaveDirectoryUri = null;
+    Uri m_saveDirectoryUri = null;
     private byte[] m_bytesToSave = null;
     ActivityResultLauncher<Intent> m_createFileLauncher = null;
 
@@ -41,25 +48,50 @@ public class ModernExternalFileManager extends ExternalFileManagerBase {
 
     @Override
     public void requestPermissions() {
-        // Nothing to do for this implementation
-        configureSaveDirectory(null);
+        m_saveDirectoryUri = null;
+        List<UriPermission> persistedPermissions =
+            m_mainActivity.getContentResolver().getPersistedUriPermissions()
+        ;
+
+        if(persistedPermissions==null || persistedPermissions.size()==0) {
+            promptUserForSaveDir();
+            return;
+        } else if(persistedPermissions.size()==1) {
+            UriPermission currentSaveDirPermission = persistedPermissions.get(0);
+            if (currentSaveDirPermission.isWritePermission()) {
+                m_saveDirectoryUri = currentSaveDirPermission.getUri();
+            } else {
+                m_mainActivity.getContentResolver().releasePersistableUriPermission(
+                    currentSaveDirPermission.getUri(),
+                    SAVEDIR_FLAGS_READ_WRITE
+                );
+                promptUserForSaveDir();
+            }
+        } else {
+            // More than one permission => something weird has happened
+            // Delete all the peristable permissions found and start again
+            for(UriPermission uriPermission: persistedPermissions) {
+                m_mainActivity.getContentResolver().releasePersistableUriPermission(
+                    uriPermission.getUri(),
+                    SAVEDIR_FLAGS_READ_WRITE
+                );
+            }
+            promptUserForSaveDir();
+        }
     }
 
-    public void configureSaveDirectory(TreeMap<String, String> permissionStatuses) {
-        m_xmlSaveDirectoryUri = null;
-
+    private void promptUserForSaveDir() {
         // ref https://stackoverflow.com/a/72404595
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
         intent.setFlags(
             // Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED |
-                Intent.FLAG_GRANT_WRITE_URI_PERMISSION |
-                Intent.FLAG_GRANT_READ_URI_PERMISSION |
+            SAVEDIR_FLAGS_READ_WRITE |
                 Intent.FLAG_GRANT_PREFIX_URI_PERMISSION |
                 Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+
         );
-        String URI_PREFIX = "content://com.android.externalstorage.documents/tree/primary%3A";
-        String URI_SUFFIX = "Documents%2F";
-        Uri initialUri = Uri.parse(URI_PREFIX + URI_SUFFIX);
+        String URI_PREFIX = "content://com.android.externalstorage.documents/tree/primary";
+        Uri initialUri = Uri.parse(URI_PREFIX );
         intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialUri);
 
         ActivityResultLauncher<Intent> directoryAccessRequestLauncher =
@@ -69,11 +101,10 @@ public class ModernExternalFileManager extends ExternalFileManagerBase {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if(result.getResultCode()== Activity.RESULT_OK ) {
-                            m_xmlSaveDirectoryUri = result.getData().getData();
+                            m_saveDirectoryUri = result.getData().getData();
                             m_mainActivity.getContentResolver().takePersistableUriPermission(
-                                m_xmlSaveDirectoryUri,
-                                Intent.FLAG_GRANT_WRITE_URI_PERMISSION |
-                                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                m_saveDirectoryUri,
+                                SAVEDIR_FLAGS_READ_WRITE
                             );
                         }
                     }
@@ -81,6 +112,10 @@ public class ModernExternalFileManager extends ExternalFileManagerBase {
             )
         ;
         directoryAccessRequestLauncher.launch(intent);
+    }
+
+    public void configureSaveDirectory(TreeMap<String, String> permissionStatuses) {
+        // no longer required
     }
 
     public void saveFile(String fileBaseName, String fileMimeType, byte[] fileContent) {

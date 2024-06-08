fe76b5490b4b1e8e8ae26d5fee0da621efec8694
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-06-08 21:28:16 +0800

    GH #8: Used Espresso to implement test of page to page navigation

--- android-app/src/androidTest/java/net/heretical_camelid/transit_emv_checker/android_app/TEC_UIAutomatorTests.java
+++ android-app/src/androidTest/java/net/heretical_camelid/transit_emv_checker/android_app/TEC_UIAutomatorTests.java
@@ -7,18 +7,39 @@ package net.heretical_camelid.transit_emv_checker.android_app;
 // Documentation on the uiautomator framework is at:
 // https://developer.android.com/training/testing/other-components/ui-automator
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.os.PatternMatcher;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.SdkSuppress;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.uiautomator.By;
@@ -37,6 +58,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
 public class TEC_UIAutomatorTests {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(TEC_UIAutomatorTests.class);
 
     private static final String TEC_ANDROID_APP_PACKAGE =
         "net.heretical_camelid.transit_emv_checker.android_app";
@@ -90,67 +113,39 @@ public class TEC_UIAutomatorTests {
 
         assertThat(mDevice, notNullValue());
 
-        // First screen displayed is the system file picker, for selection under
-        // user control of the folder to be opened as the app's documents directory
-        UiObject2 sfpSelectButton = mDevice.wait(Until.findObject(
+        Pattern buttonPattern = Pattern.compile(
+                "USE THIS FOLDER|ALLOW|SELECT|START EMV MEDIA DETECTION"
+        );
 
-            // Depending on whether this is the first run of the app after a
-            // clean install or not, the system file picker may present either
-            // a "USE THIS FOLDER" or "SELECT" button which needs to be clicked
-            // to progress.
-
-            // We would prefer to identify the button by its resource ID but
-            // this does not work (presumably because the system file picker does
-            // not make its resource id's visible in the same way as our own app).
-            // We are falling back to identifying it by displayed text, but must bear
-            // in mind that this will probably fail for a device with a non-English
-            // language setting.
-
-            // By.res(ANDROID_DOCUMENTSUI_PACKAGE, "R.id.button1")
-            By.text(Pattern.compile("SELECT|USE THIS FOLDER"))
-
-        ), _UI_APPEAR_TIMEOUT_SECONDS * 1000);
-        assertThat(sfpSelectButton, is(notNullValue()));
-        String sfpSelectButtonText = sfpSelectButton.getText();
-        sfpSelectButton.click();
-        if(sfpSelectButtonText.equals("USE THIS FOLDER")) {
-            UiObject2 allowButton = mDevice.wait(Until.findObject(
-                By.text(Pattern.compile("ALLOW"))
+        UiObject2 visibleButton;
+        String visibleButtonText;
+        do {
+            visibleButton = mDevice.wait(Until.findObject(
+                By.text(buttonPattern)
             ), _UI_APPEAR_TIMEOUT_SECONDS * 1000);
-            assertThat(allowButton, is(notNullValue()));
-            allowButton.click();
-        } else {
-            assertThat(sfpSelectButton.getText(), is(equals("SELECT")));
-        }
+            assertThat(visibleButton, is(notNullValue()));
+            visibleButtonText = visibleButton.getText();
+            LOGGER.info("Button text: " + visibleButtonText);
+            visibleButton.click();
+        } while(visibleButtonText.equals("START EMV MEDIA DETECTION") == false);
 
-        // Second screen displayed is the home screen, where the user is required
-        // to click on the 'Start Detection' button to enable NFC polling
-        UiObject2 startDetectionButton = mDevice.wait(Until.findObject(
-            //By.res(TEC_ANDROID_APP_PACKAGE, "R.id.button_home")
-            By.text("START EMV MEDIA DETECTION")
-        ), _UI_APPEAR_TIMEOUT_SECONDS);
-        assertThat(startDetectionButton, is(notNullValue()));
-        assertThat(startDetectionButton.isClickable(),is(equalTo(true)));
-        assertThat(startDetectionButton.getText(),is(equalTo("START EMV MEDIA DETECTION")));
-        startDetectionButton.click();
         sleep(_UI_CHANGE_SLEEP_SECONDS);
+
         // counter-intuitive, but button is still clickable
-        assertThat(startDetectionButton.isClickable(),is(equalTo(true)));
-        assertThat(startDetectionButton.getText(),is(equalTo("WAITING FOR EMV MEDIA")));
+        assertThat(visibleButton.isClickable(),is(equalTo(true)));
 
         // Navigate to each of the other pages in turn
-        //checkNavigationPageContent("Transit", ".*No media presented.*");
-        //checkNavigationPageContent("EMV Details", ".*No media presented.*");
-        checkNavigationPageContent("About", ".*Version.*");
+        // checkNavigationPageContent("Transit", "No media presented");
+        // checkNavigationPageContent("EMV", "No media presented");
+        checkNavigationPageContent("About", "Version");
     }
 
-    private void checkNavigationPageContent(String pageNavigationText, String expectedDisplayedRegex) {
-        Pattern expectedDisplayedPattern = Pattern.compile(expectedDisplayedRegex,Pattern.MULTILINE);
+    private void checkNavigationPageContent(String pageNavigationText, String expectedDisplayedSubstring) {
+        /*
         UiObject2 pageNavigationButton = mDevice.wait(Until.findObject(
             By.text(pageNavigationText)
         ), _UI_APPEAR_TIMEOUT_SECONDS);
 
-        /*
          * The logic above does not work yet
         assertThat(pageNavigationButton, is(notNullValue()));
         assertThat(pageNavigationButton.isClickable(),is(equalTo(true)));
@@ -160,6 +155,22 @@ public class TEC_UIAutomatorTests {
         ), _UI_APPEAR_TIMEOUT_SECONDS);
         assertThat(pageDisplayObject, is(notNullValue()));
          */
+        ViewInteraction bottomNavigationItemView3 = onView(
+                allOf(withId(R.id.navigation_about), withContentDescription("About"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView3.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withSubstring(expectedDisplayedSubstring),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.webkit.WebView.class))),
+                        isDisplayed()));
+        assertThat(textView3,is(notNullValue()));
+        // textView3.check(matches(withText(expectedDisplayedSubstring)));
     }
 
     @After
@@ -190,6 +201,25 @@ public class TEC_UIAutomatorTests {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
 

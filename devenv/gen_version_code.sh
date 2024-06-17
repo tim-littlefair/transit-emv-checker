fe824ce0c4b0c4f1a0d96fe780b86ca397aa913d
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-06-17 11:32:28 +0800

    GH #3: Including git commit hash in versionName

--- devenv/gen_version_code.sh
+++ devenv/gen_version_code.sh
@@ -9,27 +9,31 @@
 # be rendered back to hex as part of a version string.
 
 hash7_as_hex=$(git rev-parse HEAD | cut -c1-7)
-hash7_as_decimal=$(printf "%d" 0x$hash7_as_hex)
-/bin/echo git hash7 = $hash7_as_hex -\> $hash7_as_decimal
+hash7_as_decimal=$(printf "%d" "0x$hash7_as_hex")
+/bin/echo "git hash7 = $hash7_as_hex -\> $hash7_as_decimal"
 
 short_osname=$(uname -s)
 if [ "$short_osname" = "Linux" ] ; then
   sed_inplace_arg="--in-place"
 elif [ "$short_osname" = "Darwin" ] ; then
-  sed_inplace_arg="-i ''"
+  # See https://github.com/koalaman/shellcheck/wiki/SC2089
+  set -- -i ''
+  sed_inplace_arg=$0
 else
-  echo Unexpected OS name $short_osname
+  echo "Unexpected OS name $short_osname"
   return 2> /dev/null
   exit 1
 fi
 
 # For the moment we are only marking up the Android app but we could
 # apply the same change to products in other directories in the future
+# shellcheck disable=SC2043
 for d in android-app ; do
   git restore $d/build.gradle
   # Using '#' as sed delimiter to avoid a lot of escaping in file path in second command
-  /usr/bin/sed -r -e "s#versionCode [0-9]+#versionCode $hash7_as_decimal#" $sed_inplace_arg $d/build.gradle
-  /usr/bin/sed -r -e "s#replace_with_keystore_properties_path#/tmp/hc_keys/hc-playstore-upload-2024_keystore.properties#" $sed_inplace_arg $d/build.gradle
+  /usr/bin/sed -r -e "/versionName/s#(\"[0-9]+\.[0-9]+\.[0-9])+\"\$#\"\1-$hash7_as_hex\"#" "$sed_inplace_arg" $d/build.gradle
+  /usr/bin/sed -r -e "/versionCode/s#[0-9]+\$#$hash7_as_decimal#" "$sed_inplace_arg" $d/build.gradle
+  /usr/bin/sed -r -e "s#replace_with_keystore_properties_path#/tmp/hc_keys/hc-playstore-upload-2024_keystore.properties#" "$sed_inplace_arg" $d/build.gradle
 done
 
-git diff | grep -e versionCode -e keystorePropertiesFile
+git diff -U0 | grep -e versionName -e versionCode -e keystorePropertiesFile
